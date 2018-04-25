1e804151fe181b83c6d2289b61bca100fdc3cce6
ggigaev <soonglee@hotmail.com>
2018-04-24 17:56:17 -0700

    update on pipeline works

--- src/.ipynb_checkpoints/scrubbing-checkpoint.py
+++ src/.ipynb_checkpoints/scrubbing-checkpoint.py
@@ -3,36 +3,109 @@ import pandas as pd
 
 
 #####################################################################
-#  Step 1: create 'fraud_no_fraud'
+#  Step 1: remove missing violation id's
 #####################################################################
-def scrub_fraud_no_fraud(df):
+def remove_missing_vid(df):
     '''
         Input : pass in a dataframe
-        Output: returns a series of Booleans based on 'acct_type'
+        Output: returns a dataframe with clean violation id's
     '''
-    df['fraud_no_fraud'] = df['acct_type'].astype(str).str[:5] == 'fraud'
-    return df['fraud_no_fraud']
-
+    mask = df['violation_id'].isnull()
+    df_viol = df[~mask_viol]
+    
+    # clean up violation id's
+    a = df_viol['violation_id']
+    a[0].split('_')[2]
+    
+    L_vid = []
+    for id in a:
+        id_split = id.split('_')
+        vid = id_split[2]
+        L_vid.append(vid)
+        
+    df_viol['short_violation_id'] = L_vid
+    return df_viol
 
 
 #####################################################################
-#  Step 2: update time based columns to datetime
+#  Step 2: get the number of violations per business at a given 
+#          inspection date
 #####################################################################
-def scrub_datetime(df):
+def group_bid_idate(df):
     '''
         Input : pass in a dataframe
-        Output: returns a new dataframe with updated datetime
+        Output: returns a new dataframe with number of viloations per 
+                business at a given inspection date
     '''
-    df['approx_payout_date']=df['approx_payout_date'].astype("datetime64[s]")
-    df['event_created']=df['event_created'].astype("datetime64[s]")
-    df['event_end']=df['event_end'].astype("datetime64[s]")
-    df['event_published']=df['event_published'].astype("datetime64[s]")
-    df['event_start']=df['event_start'].astype("datetime64[s]")
-    df['user_created']=df['user_created'].astype("datetime64[s]")
-    return df
+    # create a datetime column for inspection date
+    df['inspect_date'] = pd.to_datetime(df.inspection_date)
+    df['short_violation_id'] = df['short_violation_id'].apply(np.int64)
+    
+    # create a pandas series with groupby
+    m1100 = df.groupby(['business_id', 'inspect_date'])['short_violation_id'].unique()
+    df_grouped = pd.DataFrame(m1100)
+    
+    # Create a frame for data filling in
+    ndf = df.drop_duplicates('business_id')
+    # idf is just a frame to be filled with data from df_grouped 
+    idf = ndf.reset_index(drop=True)
+    
+    # initialize idf
+    idf.at[:, 'p1_3'] = 0
+    idf.at[:, 'p4_6'] = 0
+    idf.at[:, 'p7_9'] = 0
+    idf.at[:, 'p10_12'] = 0
+    idf.at[:, 'p13_18'] = 0
+    idf.at[:, 'p19_24'] = 0
+    idf.at[:, 'p25_36'] = 0
+    
+    # Filling up the number of violations per business per inspection date
+    for row in df_grouped.iterrows():
+        latest_enddate = pd.to_datetime('2018-03-31')
+        bid = row[0][0]
+        date = row[0][1]
+        date1 = latest_enddate 
+        date2 = latest_enddate - pd.DateOffset(months=3)
+        date3 = latest_enddate - pd.DateOffset(months=6)
+        date4 = latest_enddate - pd.DateOffset(months=9)
+        date5 = latest_enddate - pd.DateOffset(months=12)
+        date6 = latest_enddate - pd.DateOffset(months=18)
+        date7 = latest_enddate - pd.DateOffset(months=24)
+        date8 = latest_enddate - pd.DateOffset(months=36)
+
+        if date2 < date <= date1:
+            num_viols = len(row[1]['short_violation_id'])
+            idx = idf.business_id[idf.business_id ==bid].index.tolist()
+            idf.at[idx, 'p1_3'] = num_viols
+        elif date3 < date <= date2:
+            num_viols = len(row[1]['short_violation_id'])
+            idx = idf.business_id[idf.business_id ==bid].index.tolist()
+            idf.at[idx, 'p4_6'] = num_viols
+        elif date4 < date <= date3:
+            num_viols = len(row[1]['short_violation_id'])
+            idx = idf.business_id[idf.business_id ==bid].index.tolist()
+            idf.at[idx, 'p7_9'] = num_viols
+        elif date5 < date <= date4:
+            num_viols = len(row[1]['short_violation_id'])
+            idx = idf.business_id[idf.business_id ==bid].index.tolist()
+            idf.at[idx, 'p10_12'] = num_viols
+        elif date6 < date <= date5:
+            num_viols = len(row[1]['short_violation_id'])
+            idx = idf.business_id[idf.business_id ==bid].index.tolist()
+            idf.at[idx, 'p13_18'] = num_viols
+        elif date7 < date <= date6:
+            num_viols = len(row[1]['short_violation_id'])
+            idx = idf.business_id[idf.business_id ==bid].index.tolist()
+            idf.at[idx, 'p19_24'] = num_viols
+        elif date8 < date <= date7: 
+            num_viols = len(row[1]['short_violation_id'])
+            idx = idf.business_id[idf.business_id ==bid].index.tolist()
+            idf.at[idx, 'p25_36'] = num_viols
+    
+        return idf
 
 #####################################################################
-#  Step 3: Create column for events with no organization name
+#  Step 3: Import zip codes from SF business lo
 #####################################################################
 
 def add_bool_name(df):
